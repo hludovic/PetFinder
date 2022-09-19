0a85c4bc4f5840e7cc951f7b6a104f9572f32465
Ludovic HENRY <badsteam@hotmail.com>
2022-09-18 22:06:39 -0400

    Initial Commit

---
+++ PetFinder/PetFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetFinderApp.swift
+//  PetFinder
+//
+//  Created by Ludovic HENRY on 18/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct PetFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
