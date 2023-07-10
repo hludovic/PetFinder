27381fadea663ee06d7ec9759d824026cdcc59ba
Ludovic HENRY <badsteam@hotmail.com>
2023-07-10 11:16:03 -0400

    Rebooted

--- PetFinder/PetFinderApp.swift
+++ PetFinder/PetFinderApp.swift
@@ -2,25 +2,16 @@
 //  PetFinderApp.swift
 //  PetFinder
 //
-//  Created by Ludovic HENRY on 18/09/2022.
+//  Created by Ludovic HENRY on 10/07/2023.
 //
 
 import SwiftUI
 
 @main
 struct PetFinderApp: App {
-    @Environment(\.scenePhase) private var scenePhase
-    @StateObject var aroundMeData = AroundMeVM()
-    @StateObject var model = Model()
-
     var body: some Scene {
         WindowGroup {
-            WelcomeView()
-                .environmentObject(aroundMeData)
-                .environment(\.managedObjectContext, model.localContainer.viewContext)
-                .onChange(of: scenePhase) { newLocation in
-                    if newLocation == .active { aroundMeData.startUpdatingMyLocation() }
-                }
+            ContentView()
         }
     }
 }
